e9af71469f2f994014ffce7445f2fde144d03213
Duaa-design <71189775+Duaa-design@users.noreply.github.com>
2020-10-10 01:33:42 +0300

    Initial Commit

---
+++ Duaa/DuaaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuaaApp.swift
+//  Duaa
+//
+//  Created by Duaa on 23/02/1442 AH.
+//
+
+import SwiftUI
+
+@main
+struct DuaaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
